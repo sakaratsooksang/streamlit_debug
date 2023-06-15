0cfecfec9a83dea44da9b7480c874d6e56056a95
por <binnerland2542@gmail.com>
2023-06-15 17:28:33 +0700

    add app.py

--- app.py
+++ app.py
@@ -4,27 +4,28 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-anime_df = pd.read_csv('anime_with_synopsis.csv')
-#rating_df = pd.read_csv('rating.csv')
-
-anime_df = anime_df.rename({'sypnopsis': 'Synopsis'}, axis=1)
-anime_df = anime_df.rename({'MAL_id': 'anime_id'}, axis=1)
-
-#rating_df = rating_df[(rating_df["rating"] != -1)]
-
-anime_copy = anime_df.copy()
-anime_copy = anime_copy[['Name', 'Genres', 'Synopsis']]
-anime_copy['tagline'] = anime_copy['Synopsis'] + anime_copy['Genres']
-
-tfidf = TfidfVectorizer(stop_words='english')
-anime_copy['tagline'] = anime_copy['tagline'].fillna('')
-tfidf_matrix = tfidf.fit_transform(anime_copy['tagline'])
-
 from sklearn.metrics.pairwise import linear_kernel
-cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
-def get_recommendations(Name, cosine_sim=cosine_sim):
+@st.cache_data
+def load_data():
+    anime_df = pd.read_csv('anime_with_synopsis.csv')
+    #rating_df = pd.read_csv('rating.csv')
+
+    anime_df = anime_df.rename({'sypnopsis': 'Synopsis'}, axis=1)
+    anime_df = anime_df.rename({'MAL_id': 'anime_id'}, axis=1)
+
+    #rating_df = rating_df[(rating_df["rating"] != -1)]
+
+    anime_copy = anime_df.copy()
+    anime_copy = anime_copy[['Name', 'Genres', 'Synopsis']]
+    anime_copy['tagline'] = anime_copy['Synopsis'] + anime_copy['Genres']
+
+    tfidf = TfidfVectorizer(stop_words='english')
+    anime_copy['tagline'] = anime_copy['tagline'].fillna('')
+    tfidf_matrix = tfidf.fit_transform(anime_copy['tagline'])
+    return anime_df, anime_copy, tfidf_matrix
+
+def get_recommendations(Name, cosine_sim):
     idx = anime_copy[anime_copy['Name'] == Name].index[0]
     sim_scores = sorted(list(enumerate(cosine_sim[idx])), reverse=True, key=lambda x: x[1])[1:11]
     anime_indices = [i[0] for i in sim_scores]
@@ -43,20 +44,25 @@ def Table(df):
                        align='left')))
     fig.update_layout(height=500, title ={'text': "Top 10 Anime Recommendations", 'font': {'size': 22}})
     return st.plotly_chart(fig,use_container_width=True)
-anime_list = anime_df['Name'].values
 
-st.title(':red[Anime] Recommendation System')
-selected_anime = st.selectbox(
-    "Type or select an anime from the dropdown",
-    anime_list
-)
+## main section
+if __name__ == '__main__':
+    anime_df, anime_copy, tfidf_matrix = load_data()
+    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+    anime_list = anime_df['Name'].values
 
-if st.button('Show Recommendation'):
-    recommended_anime_names = get_recommendations(selected_anime)
-    #list_of_recommended_anime = recommended_anime_names.to_list()
-   # st.write(recommended_anime_names[['title', 'description']])
-    Table(recommended_anime_names)
-    
-st.write('  '
-         )
-st.write(' ')
+    st.title(':red[Anime] Recommendation System')
+    selected_anime = st.selectbox(
+        "Type or select an anime from the dropdown",
+        anime_list
+    )
+
+    if st.button('Show Recommendation'):
+        recommended_anime_names = get_recommendations(selected_anime,cosine_sim=cosine_sim)
+        #list_of_recommended_anime = recommended_anime_names.to_list()
+    # st.write(recommended_anime_names[['title', 'description']])
+        Table(recommended_anime_names)
+        
+    st.write('  '
+            )
+    st.write(' ')
